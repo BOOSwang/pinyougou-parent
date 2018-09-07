f79246e15b2e7e13b65daebc92f3e4414d737c50
wangyanci <1973155911@qq.com>
2018-09-07 16:37:23 +0800

    测试1

---
+++ pinyougou-dao/src/main/java/com/pinyougou/mapper/Test.java
@@ -0,0 +1,19 @@
+package com.pinyougou.mapper;/**
+ * Created by wangyanci on 2018/9/7.
+ */
+
+/**
+ * &lt;pre&gt;项目名称：
+ * 类名称：
+ * 类描述：
+ * 创建人：王晏赐
+ * 创建时间：
+ * 修改人：王晏赐
+ * 修改时间：
+ * 修改备注：
+ *
+ * @version &lt;/pre&gt;
+ */
+
+public class Test {
+}
