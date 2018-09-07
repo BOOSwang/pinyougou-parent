925b488b5ed1c7539668cdb494035cb24bc376a3
wangyanci <1973155911@qq.com>
2018-09-07 21:37:40 +0800

    版本回退

---
+++ pinyougou-common/src/main/java/com/jk/TestController.java
@@ -0,0 +1,24 @@
+package com.jk;/**
+ * Created by wangyanci on 2018/9/7.
+ */
+
+/**
+ * &lt;pre&gt;项目名称：
+ * 类名称：
+ * 类描述：
+ * 创建人：王晏赐
+ * 创建时间：
+ * 修改人：王晏赐
+ * 修改时间：
+ * 修改备注：
+ *
+ * @version &lt;/pre&gt;
+ */
+
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println(544656);
+    }
+
+}
