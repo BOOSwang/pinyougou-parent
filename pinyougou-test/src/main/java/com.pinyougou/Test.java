d82aa80e4eda22f722e15331d49e452a5a24dc87
wangyanci <1973155911@qq.com>
2018-11-10 21:08:52 +0800

    上传代码测试

---
+++ pinyougou-test/src/main/java/com.pinyougou/Test.java
@@ -0,0 +1,25 @@
+package com.pinyougou;/**
+ * Created by wangyanci on 2018/11/10.
+ */
+
+/**
+ * &lt;pre&gt;项目名称：
+ * 类名称：
+ * 类描述：
+ * 创建人：王晏赐
+ * 创建时间：
+ * 修改人：王晏赐
+ * 修改时间：
+ * 修改备注：
+ *
+ * @version &lt;/pre&gt;
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("测试上传代码");
+    }
+
+
+}
